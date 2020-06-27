5ba94239050281c540e1e84706348fb2d756eee0
Bruno Gregório <Bruno08new12@gmail.com>
2020-06-27 12:08:45 -0300

    [DONE] Test files working.

---
+++ test/coretests.js
@@ -0,0 +1,17 @@
+const assert = require('chai').assert;
+const simpleloader = require('../dist/simpleloader');
+const path = require('path');
+
+describe('Find the package directory from different places', () => {
+  it('Find the package directory from here.', async () => {
+    const resultDir = await simpleloader.getPackageDir(__dirname);
+    assert.equal(resultDir, path.resolve(__dirname + '/..'));
+  });
+
+  it('Load a file from the this folder.', async () => {
+    const message = await simpleloader.loadString(
+        'test/textfile.txt',
+        __dirname);
+    assert.equal(message, 'Hello World\n');
+  });
+});
