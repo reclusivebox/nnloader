dfef10d573a0add1ad99f5ea59d88e58fa76938e
Reclusivebox <reclusivebox@outlook.com>
2020-11-08 10:12:29 -0300

    [INFO] Removed Webpack

--- webpack.development.js
+++
@@ -1,6 +0,0 @@
-const merge = require('webpack-merge');
-const main = require('./webpack.common');
-
-module.exports = merge(main,
-    {mode: 'development', devtool: 'inline-source-map'});
-
--- webpack.production.js
+++
@@ -1,5 +0,0 @@
-const merge = require('webpack-merge');
-const main = require('./webpack.common');
-
-module.exports = merge(main,
-    {mode: 'production'});
