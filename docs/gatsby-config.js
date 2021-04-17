dac71b19fb0a292c6bfda08cf00ba7f37e9f8797
Reclusive Box <reclusivebox@outlook.com>
2021-04-17 10:58:52 -0300

    Fixed inline code highlight

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -24,7 +24,12 @@ module.exports = {
     {
       resolve: 'gatsby-transformer-remark',
       options: {
-        plugins: ['gatsby-remark-prismjs']
+        plugins: [{
+          resolve: 'gatsby-remark-prismjs',
+          options: {
+            inlineCodeMarker: '&'
+          }
+        }]
       }
     },
     `gatsby-transformer-sharp`,
