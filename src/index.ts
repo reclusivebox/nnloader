05813c7233311df3159e2e4998e568564d9cdf3d
Bruno Gregório <reclusivebox@outlook.com>
2021-09-18 09:09:26 -0300

    Proxy-based approach

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,40 @@
 import {Loader, createLoader} from './Loader';
 export {createLoader, createLoaderWithPath, Loader} from './Loader';
 
-const nnLoader = createLoader();
+class LoaderStore {
+  private _nestingLevel: number = 1;
+  public loader: Loader | null = null;
+
+  set nestingLevel(value: number) {
+    if (this.loader) {
+      throw new Error("The nesting level can't be changed after a method has been called.");
+    } else {
+      this._nestingLevel = value;
+    }
+  }
+
+  get nestingLevel() {
+    return this._nestingLevel;
+  }
+}
+
+const nnLoaderStore = new LoaderStore();
+
+const handler: ProxyHandler<LoaderStore> = {
+  get: function(target, prop, receiver) {
+    if (!(prop in target)) {
+      if (target.loader) {
+        return Reflect.get(target.loader, prop, receiver);
+      } else {
+        target.loader = createLoader(1);
+        return Reflect.get(target.loader, prop, receiver);
+      }
+    } else {
+      return Reflect.get(target, prop, receiver);
+    }
+  }
+}
+
+const nnLoader = new Proxy(nnLoaderStore, handler) as unknown as Loader & {nestingLevel: number};
 
 export default nnLoader;
